b9f8c2002473b5dc636eff8ef2941d5ed24c2230
Rodrigo <rojasrodrigo95@gmail.com>
2025-11-15 20:18:22 -0300

    creacion de repositorios

---
+++ src/main/java/com/levelupgamer/backend/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.levelupgamer.backend.repositories;
+
+import com.levelupgamer.backend.models.entities.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    Optional<Categoria> findByNombreIgnoreCase(String nombre);
+}
---
+++ src/main/java/com/levelupgamer/backend/repositories/ProductoRepository.java
@@ -0,0 +1,13 @@
+package com.levelupgamer.backend.repositories;
+
+import com.levelupgamer.backend.models.entities.Categoria;
+import com.levelupgamer.backend.models.entities.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+
+    List<Producto> findByCategoria(Categoria categoria);
+}
+
