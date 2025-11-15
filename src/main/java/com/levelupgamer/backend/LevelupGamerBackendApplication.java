c76a76faf47b4a6434dc2aa0815eba5a54b2fd84
Rodrigo <rojasrodrigo95@gmail.com>
2025-11-15 19:55:31 -0300

    creacion archivos backend inicial

---
+++ src/main/java/com/levelupgamer/backend/LevelupGamerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.levelupgamer.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelupGamerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelupGamerBackendApplication.class, args);
+	}
+
+}
