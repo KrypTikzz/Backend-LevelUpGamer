a691e8a93b50296ea960dcbab7cf240a476a6dd7
Rodrigo <rojasrodrigo95@gmail.com>
2025-11-23 23:19:32 -0300

    fix controller

--- src/main/java/com/levelupgamer/backend/controllers/CategoriaController.java
+++ src/main/java/com/levelupgamer/backend/controllers/CategoriaController.java
@@ -18,6 +18,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/api/categorias")
+@CrossOrigin(origins = "*")
 public class CategoriaController {
 
     private final CategoriaRepository categoriaRepository;
